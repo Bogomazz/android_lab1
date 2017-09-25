81b59be690f1db2bf293dba3bc4fc3f87b05b366
Anton Bogomaz <bogomaz97@gmail.com>
2017-09-25 13:04:09 +0300

    Vertical and horizontal layouts for color picker.

--- app/src/main/java/com/example/macbook/lab1/MainActivity.java
+++ app/src/main/java/com/example/macbook/lab1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.macbook.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button colorDisplayButton = (Button) findViewById(R.id.colorDisplay);
+        final Button calcButton = (Button) findViewById(R.id.calc);
+        final Button notesButton = (Button) findViewById(R.id.notes);
+
+        final Intent colorDisplayIntent = new Intent(this, ColorDisplayActivity.class);
+        final Intent calcIntent = new Intent(this, CalcActivity.class);
+        final Intent notesIntent = new Intent(this, NotesActivity.class);
+
+        colorDisplayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(colorDisplayIntent);
+            }
+        });
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(calcIntent);
+            }
+        });
+        notesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(notesIntent);
+            }
+        });
     }
 }
