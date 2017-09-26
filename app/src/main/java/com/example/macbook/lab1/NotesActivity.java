f9038279f07dae87d9f0985a9e41badcb974818a
Anton Bogomaz <bogomaz97@gmail.com>
2017-09-26 18:20:26 +0300

    Added simple actionbar.

--- app/src/main/java/com/example/macbook/lab1/NotesActivity.java
+++ app/src/main/java/com/example/macbook/lab1/NotesActivity.java
@@ -2,6 +2,10 @@ package com.example.macbook.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -10,4 +14,28 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Log.v("v", "Search action clicked");
+                return true;
+            case R.id.action_add:
+                Log.v("v", "New action clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
