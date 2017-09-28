474fec1e31d32c2426241191b8732568ee46bd0e
Anton Bogomaz <bogomaz97@gmail.com>
2017-09-28 15:06:43 +0300

    Image selecting. Context menu by long click on list item. Passing data from one activity to another. Changing editNote layouts.

--- app/src/main/java/com/example/macbook/lab1/EditNoteActivity.java
+++ app/src/main/java/com/example/macbook/lab1/EditNoteActivity.java
@@ -1,13 +1,68 @@
 package com.example.macbook.lab1;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.macbook.lab1.models.Note;
+import com.example.macbook.lab1.storage.NotesStorage;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class EditNoteActivity extends AppCompatActivity {
+
+    public static final int PICK_IMAGE = 1;
+
+    private ImageView imageView;
+    private Note note = new Note();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        int id = this.getIntent().getIntExtra("id", 0);
+
+
+        if (id != -1) {
+            this.note = NotesStorage.get(id);
+        }
+
+        //ToDo: fill form fields.
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK) {
+            try{
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+
+        } else {
+            Log.v("i", "Fail");
+        }
+
     }
 }
--- app/src/main/java/com/example/macbook/lab1/NotesActivity.java
+++ app/src/main/java/com/example/macbook/lab1/NotesActivity.java
@@ -2,22 +2,29 @@ package com.example.macbook.lab1;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.macbook.lab1.models.Note;
 import com.example.macbook.lab1.storage.NotesStorage;
 import com.example.macbook.lab1.tools.NotesAdapter;
 
 public class NotesActivity extends AppCompatActivity {
+
+    ListView listView;
+    int selectedListItemPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +49,44 @@ public class NotesActivity extends AppCompatActivity {
 
         NotesAdapter na = new NotesAdapter(this, NotesStorage.get());
 
-        ListView lv = (ListView) findViewById(R.id.list);
-        lv.setAdapter(na);
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(na);
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent,
-                                    final View view,
-                                    int position, long id) {
-                Log.v("t", "" + position);
-            }
-
-        });
-
+        registerForContextMenu(listView);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        this.selectedListItemPosition = info.position;
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select The Action");
+        menu.add(0, v.getId(), 0, "Edit");//groupId, itemId, order, title
+        menu.add(0, v.getId(), 0, "Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        Log.v("menuItemId:", ""+item.getItemId());
+        if(item.getTitle()=="Edit"){
+            if (selectedListItemPosition != -1){
+                Intent intent = new Intent();
+                intent.putExtra("id", selectedListItemPosition);
+                intent.setClass(this, EditNoteActivity.class);
+                startActivity(intent);
+            }
+        }
+        else if(item.getTitle()=="Delete"){
+            if(selectedListItemPosition != -1){
+                NotesStorage.remove(selectedListItemPosition);
+            }
+        }else{
+            return false;
+        }
+        return true;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,7 +104,10 @@ public class NotesActivity extends AppCompatActivity {
                 Log.v("v", "Search action clicked");
                 return true;
             case R.id.action_add:
-                Log.v("v", "New item clicked");
+                Intent intent = new Intent();
+                intent.putExtra("id", -1);
+                intent.setClass(this, EditNoteActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
